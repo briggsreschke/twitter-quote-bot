aa7f4758826dd089f9ede4ebf4166320bc97ee52
Briggs Reschke <feature.frame@gmail.com>
2021-06-11 12:41:02 -0500

    Create quoted.py

---
+++ deep_thought/quoted.py
@@ -0,0 +1,18 @@
+# Take quote (thought) and print it to the terminal
+# chop each line off at white space so words aren't broken
+
+from deep_thought import thought
+import os
+
+ts = os.get_terminal_size()
+cols = ts.columns
+thought = thought()
+
+while(thought):
+    line = thought[:cols-1]
+    if(len(line) >= cols-1):
+        line = line.rsplit(' ', 1)[0]
+    thought = thought[len(line)+1:]
+    
+print(line)
+
