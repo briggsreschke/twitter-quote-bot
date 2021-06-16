159e468c382d748bf5d06f49b6ec9414ca63ae6c
briggsreschke <feature.frame@gmail.com>
2021-06-15 19:07:59 -0500

    Update quoted.py

--- deep_thought/quoted.py
+++ deep_thought/quoted.py
@@ -1,7 +1,7 @@
 # Take quote (thought) and print it to the terminal
 # chop each line off at white space so words aren't broken
 
-from deep_thought import thought
+#from deep_thought import thought
 import os
 
 ts = os.get_terminal_size()
