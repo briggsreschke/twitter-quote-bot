1fd520481a2817dfe1196421905689ccffcca4fa
briggsreschke <feature.frame@gmail.com>
2021-06-15 19:03:12 -0500

    Add files via upload

---
+++ deep_thought/deepbot.py
@@ -0,0 +1,21 @@
+from deepthought import *
+
+# Main ----
+LAST_N_TWEETS = 10
+FILE = "/home/briggs/Twitter/deepthought/bot/42.txt"
+
+def main():  
+    client = create_api()             
+
+    timeline_history = get_timeline_history(client, LAST_N_TWEETS)  
+    quotes = get_quotes(FILE)
+    
+    tweet = get_random(quotes)
+    while(tweet in timeline_history):
+        tweet = get_random(quotes)
+
+    client.update_status(tweet)
+    #print(tweet)
+
+if __name__ == "__main__":
+    main()
